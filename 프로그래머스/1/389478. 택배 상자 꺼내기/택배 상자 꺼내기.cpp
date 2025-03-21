96cf5c1d19dec8808cb11140f54a7145f13c0266
윤동주 <113955989+Yoondongju@users.noreply.github.com>
2025-03-21 17:07:50 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, int w, int num) {
+    int answer = 0;
+
+    while(num <= n) {
+        if(num % w == 0)
+            num++;
+        else
+            num += (w - (num % w)) * 2 + 1;        
+        answer++;
+    }
+
+    return answer;
+}
