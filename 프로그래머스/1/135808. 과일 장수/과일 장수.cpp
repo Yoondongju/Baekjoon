ce73fbe16b8973e00cd10edeefc68bdb84820d25
윤동주 <113955989+Yoondongju@users.noreply.github.com>
2025-03-22 13:36:04 +0900

    [level 1] Title: 과일 장수, Time: 22.97 ms, Memory: 34.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    
+  int scoreSize = (score.size() /  m) * m;
+    
+    sort(score.begin(), score.end() ,greater<>());
+    
+    
+    for(int i = m-1; i < scoreSize; i+= m)
+    {
+        answer += score[i] * m;
+    }
+    
+    
+    
+    return answer;
+}
